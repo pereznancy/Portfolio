d6b10f8b46187fe344036433dfff05646401f87e
pereznancy <nan.per9@outlook.com>
2018-04-29 09:25:55 -0700

    pages link and constructor started

---
+++ projects.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var newProject = function (name, image1, image2, image3, description) {
+  this.name = name;
+  this.image1 = image1;
+  this.image2 = image2;
+  this.image3 = image3;
+  this.description = description
+}
+
+var myProjects = {
+  new newProject 
+}
