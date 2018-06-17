a4fc71aad51982d2988e98c311b8a9c4887d6201
pereznancy <nan.per9@outlook.com>
2018-06-17 00:44:57 -0700

    done for now

--- public/finishedProjects.js
+++ public/finishedProjects.js
@@ -16,16 +16,16 @@ var finishedProjects = [
     name: 'Market Analysis App',
     date: 'February 2018',
     image1: 'images/busMall.jpg',
-    description: 'adlskfcjwodfawofihew'
+    description: 'check'
   },
 
   {
-    id: 'project1',
-    projectURL: 'www.aol.com',
-    name: 'Salmon Cookies',
-    date: 'February 2018',
-    image1: 'images/salmon.jpg',
-    description: 'This web application was made to help a small business as it grows. It calculates the number of cookies needed for each storefront based on anticipated customers.'
+    "id": "project1",
+    "projectURL": "www.aol.com",
+    "name": "Couch Potatoes",
+    "date": "March 2018",
+    "image1": "images/couch.jpg",
+    "description": "This web application was made to help you decide what to do on your day off, or when your regular plans have fallen through. It takes the users option selections and provides an activity to do."
   },
 
   {
@@ -34,7 +34,7 @@ var finishedProjects = [
     name: 'Market Analysis App',
     date: 'February 2018',
     image1: 'images/busMall.jpg',
-    description: 'adlskfcjwodfawofihew'
+    description: 'check check'
   }
 
 ];
